8a18280518bf7a0c772f55ece59f2b1b5fa4e227
amirulislambd <amirulislambd313@gmail.com>
2026-06-19 10:24:31 +0600

    feat(user dashboard): add Left Sidebar with valid gravity-ui icons spec

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/dashboard/user/Navbar";
+import Sidebar from "@/components/dashboard/user/Sidebar";
 import React from "react";
 
 const SidebarLayout = ({ children }) => {
   return (
     <div>
       <Navbar />
+      <Sidebar />
       {children}
     </div>
   );
